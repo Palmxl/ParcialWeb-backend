a63e3934938dfdaa92e49d27453060dcec0988a8
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-05-27 16:56:56 -0500

    ContratoController creado

--- src/main/java/parcial/web/martin/controllers/ContratoController.java
+++ src/main/java/parcial/web/martin/controllers/ContratoController.java
@@ -1,5 +1,61 @@
 package parcial.web.martin.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import parcial.web.martin.DTOs.ContratoDTO;
+import parcial.web.martin.entities.Contrato;
+import parcial.web.martin.services.ContratoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/contratos")
 public class ContratoController {
+
+    @Autowired
+    ContratoService contratoService;
+
+    @GetMapping
+    public ResponseEntity<List<ContratoDTO>> obtenerTodos() {
+        return ResponseEntity.ok(contratoService.obtenerTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContratoDTO> obtenerPorId(@PathVariable Long id) {
+        ContratoDTO contrato = contratoService.obtenerPorId(id);
+        return contrato != null ? ResponseEntity.ok(contrato) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<ContratoDTO> crearContrato(@RequestBody Contrato contrato) {
+        return ResponseEntity.ok(contratoService.crearContrato(contrato));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ContratoDTO> actualizarContrato(@PathVariable Long id, @RequestBody Contrato contrato) {
+        ContratoDTO contratoActualizado = contratoService.actualizarContrato(id, contrato);
+        
+        return contratoActualizado != null ? ResponseEntity.ok(contratoActualizado) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarContrato(@PathVariable Long id) {
+        return contratoService.eliminarContrato(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
+
+
 
 }
